c425595b4502e3efc61bf0135472d747a101775b
Lawjay Lee <lawjaylee1@icloud.com>
2026-07-12 21:18:30 -0700

    basic form

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,30 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button';
+	import SunIcon from '@lucide/svelte/icons/sun';
+	import MoonIcon from '@lucide/svelte/icons/moon';
+
+	import { toggleMode } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<ModeWatcher />
+
+<div class="min-h-svh flex flex-col gap-4 items-center p-4">
+	<header>
+		<Button onclick={toggleMode} variant="outline" size="icon" class="cursor-pointer">
+			<SunIcon
+				class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90"
+			/>
+			<MoonIcon
+				class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
+	</header>
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,64 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Card from '$lib/components/ui/card/card.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import Label from '$lib/components/ui/label/label.svelte';
+
+	let entry = $state(0);
+	let stop = $state(0);
+	let exit = $state(0);
+
+	function formatCents(cents: number) {
+		return (cents / 100).toFixed(2);
+	}
+
+	function parseDigits(value: string): number {
+		const digits = value.replace(/\D/g, '');
+		return digits ? Number.parseInt(digits, 10) : 0;
+	}
+</script>
+
+<Card class="w-lg p-8 bg-card-fg">
+	<div>
+		<Label for="entry" class="mb-1">Entry</Label>
+		<Input
+			id="entry"
+			type="text"
+			inputmode="numeric"
+			autocomplete="off"
+			value={formatCents(entry)}
+			oninput={(e) => {
+				entry = parseDigits(e.currentTarget.value);
+			}}
+		/>
+	</div>
+	<div>
+		<Label for="stop" class="mb-1">Stop</Label>
+		<Input
+			id="stop"
+			type="text"
+			inputmode="numeric"
+			autocomplete="off"
+			value={formatCents(stop)}
+			oninput={(e) => {
+				stop = parseDigits(e.currentTarget.value);
+			}}
+		/>
+	</div>
+	<div>
+		<Label for="exit" class="mb-1">Exit</Label>
+		<Input
+			id="exit"
+			type="text"
+			inputmode="numeric"
+			autocomplete="off"
+			value={formatCents(exit)}
+			oninput={(e) => {
+				exit = parseDigits(e.currentTarget.value);
+			}}
+		/>
+	</div>
+	<output>
+		<Label for="maxshares" class="mb-1">Max Shares</Label>
+		<Input id="maxshares" type="number" readonly value={entry + stop + exit} />
+	</output>
+</Card>
